879e6e1ffe9938a6f53623e3a2485d33c27e1293
Mikhail Trunov <mikhail.trunov@astraia.com>
2023-01-04 12:37:50 +0100

    Update dict structure

--- models/dictionar.py
+++ models/dictionar.py
@@ -1,6 +1,6 @@
 from dbutils.dbconnect import Base
 from sqlalchemy import Column, Integer, PrimaryKeyConstraint
-from sqlalchemy.types import VARCHAR, FLOAT
+from sqlalchemy.types import VARCHAR, FLOAT, BOOLEAN, SMALLINT
 
 
 class Dictionar(Base):
@@ -15,9 +15,26 @@ class Dictionar(Base):
     maximum = Column('maximum', FLOAT, nullable=True)
     dlist = Column('list', VARCHAR(length=255), nullable=True)
     dnatip = Column('dna_tip', Integer, nullable=True)
-
-
+    zeroallowed = Column('zeroallowed', BOOLEAN, nullable=True)
+    required = Column('required', BOOLEAN, nullable=True)
+    anomaly = Column('anomaly', BOOLEAN, nullable=True)
+    length = Column('length', SMALLINT, nullable=True)
+    global_prop = Column('global', BOOLEAN, nullable=True)
+    dna = Column('dna', Integer, nullable=True)
+    dna_chart = Column('dna_chart', Integer, nullable=True)
+    minvalue = Column('minvalue', FLOAT, nullable=True)
+    maxvalue = Column('maxvalue', FLOAT, nullable=True)
+    special = Column('special', SMALLINT, nullable=True)
     units = Column('units', Integer, nullable=True)
+    flagged = Column('flagged', BOOLEAN, nullable=True)
+    dna_flag = Column('dna_flag', Integer, nullable=True)
+    flag_expression = Column('flag_expression', VARCHAR(length=2000), nullable=True)
+    blocks = Column('blocks', VARCHAR(length=255), nullable=True)
+    block_multi = Column('block_multi', BOOLEAN, nullable=True)
+    flag_link = Column('flag_link', VARCHAR(length=255), nullable=True)
+    int_precision = Column('int_precision', SMALLINT, nullable=True)
+    dec_precision = Column('dec_precision', SMALLINT, nullable=True)
+    existing_value_changeable = Column('existing_value_changeable', VARCHAR(length=512), nullable=True)
 
     __tablename__ = 'dictionary'
     __table_args__ = (
--- schemas/dictionar.py
+++ schemas/dictionar.py
@@ -14,12 +14,32 @@ class DictionarBase(BaseModel):
     maximum: Union[float, None] = None
     dlist: Union[str, None] = None
     dnatip: Union[int, None] = None
+    zeroallowed: Union[bool, None] = None
+    required: Union[bool, None] = None
+    anomaly: Union[bool, None] = None
+    length: Union[str, None] = None
+    global_prop: Union[bool, None] = None
+    dna: Union[str, None] = None
+    dna_chart: Union[str, None] = None
+    minvalue: Union[float, None] = None
+    maxvalue: Union[float, None] = None
+    special: Union[str, None] = None
     units: Union[int, None] = None
+
+    flagged: Union[bool, None] = None
+    dna_flag: Union[int, None] = None
+    flag_expression: Union[str, None] = None
+    blocks: Union[str, None] = None
+    block_multi: Union[bool, None] = None
+    flag_link: Union[str, None] = None
+    int_precision: Union[int, None] = None
+    dec_precision: Union[int, None] = None
+    existing_value_changeable: Union[str, None] = None
+
 
     class Config:
         orm_mode = True
         allow_population_by_field_name = True
-
 
 class DictionarList(BaseModel):
 
